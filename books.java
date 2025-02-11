e06d6255a6bd900f626d4bf5cc5bf00efed1fab0
krishna-s19 <vamankrishna123@gmail.com>
2025-02-11 21:46:01 +0530

    books.java merging connecpt

--- books.java
+++ books.java
@@ -1,6 +1,7 @@
 Class Books{
 Public static void main(String args[] ){
-system.out.println("Have a color full day");
+system.out.println("Have a color full day this is merging option");
+system.out.println("New line added ");
 
 }
 }
