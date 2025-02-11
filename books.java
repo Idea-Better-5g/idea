201bca38a3c586330649f39018eba815ce796f32
krishna-s19 <vamankrishna123@gmail.com>
2025-02-11 22:29:49 +0530

    devops updated code

--- books.java
+++ books.java
@@ -1,6 +1,6 @@
 Class Books{
 Public static void main(String args[] ){
-system.out.println("Have a color full day this is merging option");
+system.out.println("Hi good evening, shall we pracyices above problems");
 system.out.println("New line added ");
 
 }
